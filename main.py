442276b0079909da03b8a25611b4f443800078d4
GayFarcore <farcore69@gmail.com>
2025-05-03 18:34:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands, ui, Interaction, Thread
 import asyncio
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
